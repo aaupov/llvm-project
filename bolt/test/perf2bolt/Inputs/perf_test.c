0ad56d27f03c1c79a135e31526a5eebe24e84f73
Amir Ayupov <amir.aupov@gmail.com>
2025-12-29 15:00:07 -0800

    [BOLT][test] Fix perf_test

--- bolt/test/perf2bolt/Inputs/perf_test.c
+++ bolt/test/perf2bolt/Inputs/perf_test.c
@@ -1,22 +1,7 @@
-int add(int a, int b) { return a + b; }
-int minus(int a, int b) { return a - b; }
-int multiple(int a, int b) { return a * b; }
-int divide(int a, int b) {
-  if (b == 0)
-    return 0;
-  return a / b;
-}
+#include <signal.h>
+#include <unistd.h>
 
 int main() {
-  int a = 16;
-  int b = 8;
-
-  for (int i = 1; i < 1000000; i++) {
-    add(a, b);
-    minus(a, b);
-    multiple(a, b);
-    divide(a, b);
-  }
-
+  kill(getpid(), SIGUSR2);
   return 0;
 }
